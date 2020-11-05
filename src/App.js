efda7c0ffc84f375ee9094d1e3db26c46de4a216
blackparrot <ghost135792468@gmail.com>
2020-11-05 13:57:04 +0200

    create simple finder app..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, {useEffect, useReducer} from "react";
+
 import './App.css';
+import Header from "./components/header/header.component";
+import FilmsList from "./components/films-list/films-list.component";
+import Search from "./components/search/search.component";
+import {filmReducer, INITIAL_STATE} from "./reducer/film/film.reducer";
+import {addFilms, changeInputValue} from "./reducer/film/film.actions";
+import {FilmsData} from "./films-data/films-data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [state, dispatch] = useReducer(filmReducer, INITIAL_STATE)
+
+    useEffect(() => {
+        dispatch(addFilms(FilmsData))
+    }, [])
+
+    const handleChange = event => {
+        dispatch(changeInputValue(event.target.value))
+    }
+
+    const filteredMovies = state.films.filter(film => film.name.toLowerCase().includes(state.string))
+
+    return (
+        <div className="App">
+            <Header/>
+            <Search
+                placeholder={"find film"}
+                handleChange={handleChange}
+                value={state.string}
+            />
+            <FilmsList films={filteredMovies}/>
+        </div>
+    );
 }
 
 export default App;
